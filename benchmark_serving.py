d976ec859a922ca27e627c3034fd3580be09a19a
Brendan Slabe <slabe@google.com>
2025-03-12 21:51:58 +0000

    refactor

--- benchmark_serving.py
+++ benchmark_serving.py
@@ -477,26 +477,21 @@ async def benchmark(
             continue
         latency, ttft, itl, errors = res
         if errors:
-            for k, v in errors.items():
-                overall_results["errors"][k] += v
+          for k, v in errors.items():
+              overall_results["errors"][k] += v
+              per_model_results[chosen_model]["errors"][k] += v
         else:
           prompt_len, output_len, request_latency = latency
           overall_results["latencies"].append(latency)
+          per_model_results[chosen_model]["latencies"].append(latency)
           if ttft:
               overall_results["ttfts"].append(ttft)
               overall_results["tpots"].append((request_latency - ttft) / (output_len - 1) if output_len > 1 else 0)
-          if itl:
-              overall_results["itls"].extend(itl)
-
-          per_model_results[chosen_model]["latencies"].append(latency)
-          if ttft:
               per_model_results[chosen_model]["ttfts"].append(ttft)
               per_model_results[chosen_model]["tpots"].append((request_latency - ttft) / (output_len - 1) if output_len > 1 else 0)
           if itl:
-              per_model_results[chosen_model]["itls"].extend(itl)
-          if errors:
-              for k, v in errors.items():
-                  per_model_results[chosen_model]["errors"][k] += v
+              overall_results["itls"].extend(itl)     
+              per_model_results[chosen_model]["itls"].extend(itl)     
 
     benchmark_duration = time.time() - benchmark_start_time
     
