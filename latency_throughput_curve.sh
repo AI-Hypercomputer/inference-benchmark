945cd1b01d1d6b874ebaeafc7685189ad76c285d
Brendan Slabe <slabe@google.com>
2025-03-27 21:43:27 +0000

    more reversions

--- latency_throughput_curve.sh
+++ latency_throughput_curve.sh
@@ -38,7 +38,7 @@ for request_rate in $(echo $REQUEST_RATES | tr ',' ' '); do
   fi
 
   echo "TOTAL prompts: $num_prompts"
-  
+
   # Build the python command options
   PYTHON_OPTS="$PYTHON_OPTS --save-json-results --host=$IP --port=$PORT --dataset=$PROMPT_DATASET_FILE --tokenizer=$TOKENIZER --request-rate=$request_rate --backend=$BACKEND --num-prompts=$num_prompts --max-input-length=$INPUT_LENGTH --max-output-length=$OUTPUT_LENGTH --file-prefix=$FILE_PREFIX --models=$MODELS --pm-namespace=$PM_NAMESPACE --pm-job=$PM_JOB"
 
@@ -62,7 +62,7 @@ for request_rate in $(echo $REQUEST_RATES | tr ',' ' '); do
   if [[ "$OUTPUT_BUCKET_FILEPATH" ]]; then
     PYTHON_OPTS="$PYTHON_OPTS --output-bucket-filepath $OUTPUT_BUCKET_FILEPATH"
   fi
-
+  
   $PYTHON $PYTHON_OPTS > $output_file
   cat $output_file
   sleep 30 # wait 30 seconds before next run to ensure metrics isolation
